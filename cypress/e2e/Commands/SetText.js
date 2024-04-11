c3ea3526ca8af6ea294ff965952a9e7a465fc925
SarvY1999 <sarveshy193@gmail.com>
2024-04-11 22:55:02 +0530

    Adds updated setText command

--- cypress/e2e/Commands/SetText.js
+++ cypress/e2e/Commands/SetText.js
@@ -1,4 +1,4 @@
 export function setText(ref, text) {
-    cy.get(ref).type(text)
-    cy.get(ref).should('contain.value', text)
+    cy.get(ref).first().type(text)
+    cy.get(ref).first().should('contain.value', text)
 }
