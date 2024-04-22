3c49ffb16f89e4c396c2ddc3f9f28010c13acf9f
SarvY1999 <sarveshy193@gmail.com>
2024-04-22 21:58:41 +0530

    updated SaveRecord command

--- cypress/e2e/Commands/SaveRecord.js
+++ cypress/e2e/Commands/SaveRecord.js
@@ -1,6 +1,6 @@
 /// < references type = "Cypress" /> ///
 export function saveRecord() {
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').first().click();
     cy.wait(5000);
     //div.oxd-loading-spinner
 }
