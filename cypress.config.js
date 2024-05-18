17166698eadbe7d52a7658547e5f680434e81f09
SarvY1999 <sarveshy193@gmail.com>
2024-05-18 15:52:54 +0530

    Adds mochawsome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = defineConfig({
   e2e: {
     specPattern: "**/*.feature",
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('file:preprocessor', cucumber());
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
